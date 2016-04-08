b69d61b421290462a0e786e4751bf9371f39c461
Joshua Kegley <josh1289op@gmail.com>
2016-04-08 17:30:26 -0400

    Fixed Project Name

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -17,4 +17,23 @@ exports.render = function(req, res) {
 	}
 };
 
+exports.render = function(req, res) {
 
+	//res.render('index', {
+		//title: 'Home',
+		//user: req.user ? req.user.username : ''
+	//});
+
+	if (!req.user) {
+		//tell the login that you wanna login but you want to login to a particular lobby
+		res.redirect('/login/'+req.lobby);
+	}
+
+	else {
+		res.render('index', {
+			title: 'Home',
+			user: req.user ? req.user.username : 'error'
+			
+		});
+	}
+};
